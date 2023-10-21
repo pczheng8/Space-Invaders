e985c9829865daeec3d908cc4150aac001fe21e9
pczheng8 <121399938+pczheng8@users.noreply.github.com>
2023-10-21 22:35:35 +0000

    fix code formatting

--- Ship.java
+++ Ship.java
@@ -11,24 +11,10 @@ public class Ship extends MovableObject {
     {
     	super(mainPanel, filename, image, x, y, width, height);
  	}
-
-	// FINISH ME
-	// finish writing the move() method
-	// since it was abstract in the MovableObject class
-	
- 	// this method is an example of polymorphism
- 	// since in the main program it is of type
- 	// MovableObject ship;  (a super class reference)
- 	// but yet at runtime it will call this method,
- 	//    NOT the method in the MovableObject class
- 	// this method overrides the same method in the MovableObject class
- 	
  	 
     public void move()
     {
-    	int dir = getDirection(); // where is this defined???????
-    	// this should return 90 degrees or 270 degrees
-    	// Look for methods to call in the super class
+    	int dir = getDirection();
 
       if(dir==90)
         moveRight();
